bbb853bec64662a1cabc2d24a85d6e3300ba0597
Margy Hockett <154528179+mhockett@users.noreply.github.com>
2023-12-26 10:51:05 -0500

    Update testChangesetSetupPg.sh

--- testChangesetSetupPg.sh
+++ testChangesetSetupPg.sh
@@ -6,7 +6,9 @@ export changelogSchema=schemaName
 export pgDefaultSchema="defaultSchema"
 
 javac -cp 'lib/*' src/TestLiquibaseScope.java
+javac -cp 'lib/*' src/TestLiquibaseScope2.java
 javac -cp 'lib/*' src/TestLiquibaseUpdate.java
 cd src
-java -cp '../lib/*:.' -Dliquibase.logLevel=FINE  -Dlogback.configurationFile='../logback.xml'  TestLiquibaseUpdate  ../testChangeset ${changelogSchem} &>updatelog.log
-java -cp '../lib/*:.' -Dliquibase.logLevel=FINE -DsearchPath='../testChangeset'  -Dlogback.configurationFile='../logback.xml'  TestLiquibaseScope  ../testChangeset ${changelogSchem} &>scopelog.log
+java -cp '../lib/*:.' -Dliquibase.logLevel=FINE  -Dlogback.configurationFile='../logback.xml'  TestLiquibaseUpdate  ../testChangeset ${changelogSchema} &>updatelog.log
+java -cp '../lib/*:.' -Dliquibase.logLevel=FINE -DsearchPath='../testChangeset'  -Dlogback.configurationFile='../logback.xml'  TestLiquibaseScope  ../testChangeset ${changelogSchema} &>scopelog.log
+java -cp '../lib/*:.' -Dliquibase.logLevel=FINE -DsearchPath='../testChangeset'  -Dlogback.configurationFile='../logback.xml'  TestLiquibaseScope2  ../testChangeset ${changelogSchema} &>scopelog2.log
